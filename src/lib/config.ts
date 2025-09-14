480a1ee30e80717071490626a37801117d8413cc
ssreddy1989 <ssreddyfeb17@gmail.com>
2025-09-14 22:02:07 +0530

    Initial commit - Medusa storefront

---
+++ src/lib/config.ts
@@ -0,0 +1,10 @@
+import Medusa from "@medusajs/js-sdk"
+
+const MEDUSA_BACKEND_URL =
+  process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL ?? "http://localhost:9000"
+
+export const sdk = new Medusa({
+  baseUrl: MEDUSA_BACKEND_URL,
+  debug: process.env.NODE_ENV === "development",
+  publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY,
+})
